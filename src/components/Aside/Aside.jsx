62d7dfdd0315d710a45d50167dd6059e06b0215e
Foster Horak <fosterhorak@macbook-pro.attlocal.net>
2021-01-31 05:38:01 -0600

    css

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -16,9 +16,9 @@ export default function Aside({ user, setUser }) {
                 <hr/>
             </div>
             <div className="aside-nav">
+                <Link exact to="/new-log"> <button id="nav-btn"> NEW LOG </button></Link>
                 <Link exact to="/logs"> <button id="nav-btn">EXERCISE LOGS</button> </Link>
                 <Link exact to="/exercises"><button id="nav-btn">EXERCISES</button></Link>
-                <Link exact to="/new-log"> <button id="nav-btn"> NEW LOG </button></Link>
                 <Link exact to="/new-exercise"><button id="nav-btn"> NEW EXERCISE</button> </Link>
             </div>
             <div className="aside-logout">
--- src/components/exerciseComponents/ExerciseListPanel/ExerciseListCard/ExerciseListCard.jsx
+++ src/components/exerciseComponents/ExerciseListPanel/ExerciseListCard/ExerciseListCard.jsx
@@ -9,7 +9,7 @@ export default function ExerciseListCard({ exercise, activeExercise, setActiveEx
   return (
     <>
         <div onClick={() => setActiveExercise(exercise)} className={exercise === activeExercise ? "exercise-card selected" : "exercise-card"}>
-            <h1>{exercise.name}</h1> 
+            <h1 className="card-title" >{exercise.name}</h1> 
             <p className="b"><span > {exercise.category.toUpperCase()} </span></p>
         </div>
 
